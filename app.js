6d14a79c519db1ad73806e9af7242f6e4efa5fc1
Alexnortung <alex_nortung@live.dk>
2018-04-04 00:29:57 +0200

    minor changes

--- app.js
+++ app.js
@@ -42,12 +42,35 @@ const comments = client.CommentStream(streamOpts);
 comments.on("comment", (comment) => {
 	//console.log(comment);
 
+	if (comment.author.name.toLowerCase() == process.env.CLIENT_ID.toLowerCase()) {
+		return;
+	}
+
 	if (mySubmissions.indexOf(comment.link_id) != -1) {
 		console.log("comment on my submission");
 		comment.upvote();
 		if (comment.link_id == comment.parent_id) {
-			console
-			comment.reply("Thanks! Your comment has been upvoted");
+			
+			comment.author.getSubmissions().then((submissions) => {
+				var postsUpvoted = 0;
+				submissions.forEach((submission) => {
+					//console.log(submission);
+					submission.upvote().then(() => {
+						postsUpvoted++;
+					}).catch(() => {
+						//already upvoted
+					})
+				});
+				var str;
+				if (postsUpvoted == 0) {
+					str = "Thanks! Your comment has ben upvoted";
+				} else {
+					"Thanks! Your comment and " + postsUpvoted + " of your posts has been upvoted!"
+				}
+				
+				comment.reply(str);
+			});
+			
 		}
 		
 	}
